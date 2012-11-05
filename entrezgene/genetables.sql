b1f76b69e5f86671b8a166e66335ceeac4f8eaf9
Adrian Heilbut <heilbut@broadinstitute.org>
2012-11-05 17:36:48 -0500

    starting to get d3 browser for pd data to actually work...

--- entrezgene/genetables.sql
+++ entrezgene/genetables.sql
@@ -35,3 +35,23 @@ CREATE TABLE gene2acc (
 create index genesymbol_i on geneinfo(symbol);
 create index geneid_id on geneinfo(gene_id);
 
+CREATE TABLE generif_intx (
+ tax_id_a INTEGER,
+ gene_id_a INTEGER,
+ pmid text
+ tax_id_b INTEGER,
+ gene_id_b INTEGER
+)
+
+INSERT INTO generif_intx
+SELECT a.tax_id, a.gene_id, a.pmid, b.tax_id, b.gene_id
+ FROM generifs a INNER JOIN generifs b ON 
+ a.pmid = b.pmid AND a.gene_id != b.gene_id 
+
+
+INSERT INTO intx_count
+select iA.symbol, iB.symbol, COUNT(DISTINCT pmid) from generif_intx
+ INNER JOIN geneinfo iA ON tax_id_a = iA.tax_id AND gene_id_a = iA.gene_id
+ INNER JOIN geneinfo iB ON tax_id_b = iB.tax_id AND gene_id_b = iB.gene_id
+ GROUP BY iA.symbol, iB.symbol
+
