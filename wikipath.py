0ffa15863d6776ba82f0cd0196231df6e55d2e2f
Adrian Heilbut <heilbut@broadinstitute.org>
2013-02-08 11:57:36 -0500

    added overlaps page

--- wikipath.py
+++ wikipath.py
@@ -8,6 +8,10 @@ from lxml import etree
 import pandas as pd
 import glob
 
+import scipy.stats
+import statsmodels
+import statsmodels.sandbox
+
     
 class WikiPathSets():
     def __init__(self):
@@ -149,8 +153,29 @@ def get_overlaps(test_set, geneset_dict):
                                sum([s in set_contents for s in set(symbol_list)]),
                                symbol_set.intersection(set_contents), len(set_contents) ) )
 
-            overlaps = pd.DataFrame(match_set, columns = ["set_name", "match_count", "intersect_list", "total"])
+            overlaps = pd.DataFrame(match_set, columns = ["set_name", "match_count", "intersect_list", "size of set"])
     return overlaps 
+
+
+def calc_overlap_stats(test_set, geneset_dict, total_genes):
+    """ get the overlaps and compute hypergeometric stats""" 
+
+    overlaps = get_overlaps(test_set, geneset_dict)
+
+    p = overlaps.apply( lambda x: (
+      scipy.stats.hypergeom.sf(
+        x.ix["match_count"]-1, # number of differentially expressed genes in set
+        total_genes, # total number of genes
+        x.ix["size of set"], # number of genes in current set
+        len( test_set ))), # total number of differentially expressed genes
+     axis=1)
+    p = pd.DataFrame(p, columns=["hypergeom p-val"])
+
+    overlaps = overlaps.merge(p, left_index=True, right_index=True).sort("hypergeom p-val", ascending=True)
+    overlaps["bonferroni"] = statsmodels.sandbox.stats.multicomp.multipletests(overlaps.ix[:,"hypergeom p-val"], method="bonferroni")[1]
+    overlaps["b-h fdr adj pval"] = statsmodels.sandbox.stats.multicomp.multipletests(overlaps.ix[:,"hypergeom p-val"].fillna(1.0), method="fdr_bh")[1]
+
+    return overlaps.sort("hypergeom p-val", ascending=True)
 
 """
 
