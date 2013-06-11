45707113f8b3ab2fe460cb9adcdcc7a15f5fd2f0
Adrian Heilbut <heilbut@broadinstitute.org>
2013-06-11 18:45:09 -0400

    added boxplots

--- wikipath.py
+++ wikipath.py
@@ -12,6 +12,8 @@ import scipy.stats
 import statsmodels
 import statsmodels.sandbox
 
+import statsmodels.sandbox.stats.multicomp as multicomp
+
 import alib_local
     
 class WikiPathSets():
@@ -158,7 +160,11 @@ def get_overlaps(test_set, geneset_dict):
                                sum([s in set_contents for s in set(symbol_list)]),
                                symbol_set.intersection(set_contents), len(set_contents) ) )
 
-            overlaps = pd.DataFrame(match_set, columns = ["set_name", "match_count", "intersect_list", "size of set"])
+            overlaps = pd.DataFrame(match_set, 
+                                    columns = ["set_name", 
+                                               "match_count", 
+                                               "intersect_list", 
+                                               "size of set"])
     return overlaps 
 
 
@@ -169,19 +175,24 @@ def calc_overlap_stats(test_set, geneset_dict, total_genes):
 
     p = overlaps.apply( lambda x: (
       scipy.stats.hypergeom.sf(
-        x.ix["match_count"]-1, # number of differentially expressed genes in set
-        total_genes, # total number of genes
-        x.ix["size of set"], # number of genes in current set
-        len( test_set ))), # total number of differentially expressed genes
-     axis=1)
+          x.ix["match_count"]-1, # number of differentially expressed genes in set
+          total_genes,           # total number of genes
+          x.ix["size of set"],   # number of genes in current set
+          len( test_set ))),     # total number of genes in test set
+                               axis=1)
     p = pd.DataFrame(p, columns=["hypergeom p-val"])
-
+    
     overlaps = overlaps.select(lambda x: overlaps.ix[x, "match_count"] > 0)
-    overlaps = overlaps.merge(p, left_index=True, right_index=True).sort("hypergeom p-val", ascending=True)
+    overlaps = overlaps.merge(p, 
+                              left_index=True, 
+                              right_index=True).sort("hypergeom p-val", ascending=True)
 
     if len(overlaps.index) > 0:
-        overlaps["bonferroni"] = statsmodels.sandbox.stats.multicomp.multipletests(overlaps.ix[:,"hypergeom p-val"], method="bonferroni")[1]
-        overlaps["b-h fdr adj pval"] = statsmodels.sandbox.stats.multicomp.multipletests(overlaps.ix[:,"hypergeom p-val"].fillna(1.0), method="fdr_bh")[1]
+        overlaps["bonferroni"] = multicomp.multipletests(overlaps.ix[:,"hypergeom p-val"], 
+                                                         method="bonferroni")[1]
+        overlaps["b-h fdr adj pval"] = multicomp.multipletests(
+            overlaps.ix[:,"hypergeom p-val"].fillna(1.0), 
+            method="fdr_bh")[1]
 
     return overlaps.sort("hypergeom p-val", ascending=True)
 
